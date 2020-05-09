b48218d164f87dfebdbafa41af7dbb6ce7801ae1
Thomasr <thomas.ribeiro.ext@synchro.com.br>
2020-05-09 17:34:02 -0300

    add struct html

--- conversor-temp-using-hooks/src/components/convertTemp.js
+++ conversor-temp-using-hooks/src/components/convertTemp.js
@@ -3,94 +3,73 @@ import React, { useEffect, useState } from "react";
 import "./convertTemp.css";
 
 export default function ConvertTemp() {
-  const [unitA, setUnitA] = useState(["Celsius"]);
-  const [unitB, setUnitB] = useState(["Celsius"]);
-  const [tempA, setTempA] = useState([]);
-  const [tempB, setTempB] = useState([]);
+  const [unitA, setUnitA] = useState("Celsius");
+  const [unitB, setUnitB] = useState("Celsius");
+  const [tempA, setTempA] = useState(0);
 
-  function converterTemperature() {
+  function convertTemperature() {
+    var tempB = document.getElementById("tempB");
+    console.log('undi A:'+unitA+' unit b:'+unitB)
     if (unitA == "Celsius") {
       if (unitB == "Fahrenheit") {
-        let aux = celsiusToFahrenheit(tempA);
-        setTempB(aux);
-        console.log(tempB);
+        //conversao de celsius para celsius
+        console.log(celsiusToFahrenheit(tempA))
+        tempB.value = celsiusToFahrenheit(tempA);
+        console.log(tempB.value);
       }
-      if (unitB == "Kelvin") {
-        setTempB(celsiusToKelvin(tempA));
+      if ("unitB" =="Kelvin") {
+        console.log('ete')
+        
+        //converter de celsius para fahrenheit
       } else {
-        console.log("celsiu to celsiu");
-        setTempB(tempA);
-        console.log("temp a :" + tempA + "temp b " + tempB);
+        tempB.value = tempA;
+        //converter de celsius para kelvin
       }
-    }
-    if (unitB == "Kelvin") {
     }
   }
 
   function celsiusToFahrenheit(value) {
-    console.log(value * 1.8 + 32);
-
-    return value * 1.8 + 32;
-  }
-  function celsiusToKelvin(value) {
-    setTempB(0);
-    console.log(value + 273.15);
-    return value + 273.15;
-  }
-  /*   function fahrenheitToCelsius(value) {
-    return ((value - 32) * 5) / 9;
-  }
-  function fahrenheitToKelvin(value) {
-    return ((value + 459.67) * 5) / 9;
-  }
-  function kelvinToCelsius(value) {
-    return value - 273.15;
-  }
-  function kelvinToFahrenheit(value) {
-    return value * 1.8 - 459.67;
-  } */
-
+    return (value * 1.8 + 32) ;
+}
   useEffect(() => {
-    converterTemperature();
-  }, [tempA, tempB, unitA, unitB]);
-
+    convertTemperature();
+  }, [unitA, unitB, tempA]);
   return (
     <>
-      <h1>Conversor de temperatura</h1>
       <div id="container">
+        <h1>Conversor de temperatura</h1>
+        <h2>Using hooks</h2>
         <div id="blockA">
           <input
             type="number"
-            id="tempA"
-            name="tempA"
             onChange={(e) => {
               setTempA(e.target.value);
             }}
-          />
+          ></input>
           <select
-            id="unitA"
-            name="unitA"
-            onChange={(e) => setUnitA(e.target.value)}
+            id="uintA"
+            onChange={(e) => {
+              setUnitA(e.target.value);
+            }}
           >
-            <option value="Celsius">Grau Celsius</option>
-            <option value="Fahrenheit">Grau Fahrenheit</option>
-            <option value="Kelvin">Kelvin</option>
+            <option value="Celsius"> Celsius</option>
+            <option value="Fahrenheit"> Fahrenheit</option>
+            <option value="Kelvin"> Kelvin</option>
           </select>
         </div>
-        <span name="unit2" id="equal">
-          =
-        </span>
-
+        <span id="equal"> = </span>
         <div id="blockB">
-          <input type="number" id="tempB" onChange={(e)=>{setUnitB(e.target.value)}} value={tempB} />
+          <input type="number" id="tempB" disabled="disabled" name="tempB" />
+
           <select
-            id="unitB"
-            name="unitB"
-            onChange={(e) => setUnitB(e.target.value)}
+            id="uintB"
+            onChange={(e) => {
+              setUnitB(e.target.value);
+            }}
           >
-            <option value="Celsius">Grau Celsius</option>
-            <option value="Fahrenheit">Grau Fahrenheit</option>
-            <option value="Kelvin">Kelvin</option>
+            <option value="Celsius"> Celsius</option>
+            <option value="Fahrenheit"> Fahrenheit</option>
+            <option value="Kelvin"> Kelvin</option>
           </select>
         </div>
       </div>
